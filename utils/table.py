6b9159d2edd56690bce0e25d7fffa5e5afbcd011
Nico Dnb <nico.deneubourg@gmail.com>
2023-09-18 12:41:33 +0200

    Added docstrings and updated assign_seat method

--- utils/table.py
+++ utils/table.py
@@ -6,10 +6,18 @@ class Seat():
         self.occupant: str = ""
     
     def set_occupant(self, name: str):
+        """
+        Assign a person to a free seat
+        """
+        
         if self.free == True:
             self.occupant == name
     
     def remove_occupant(self) -> str:
+        """
+        Remove a person from a seat
+        """
+
         if self.free == False:
             name = self.occupant
             self.occupant = ""
@@ -21,18 +29,30 @@ class Table():
         self.seats: List[Seat] = self.seats
 
     def has_free_spot(self) -> bool:
+        """
+        Check if at least one seat is free at the table
+        """
+
         for seat in self.seats:
             if seat.free == True:
                 return True
         return False
 
     def assign_seat(self, name: str):
+        """
+        Assign someone to a seat at the table
+        """
+
         for seat in self.seats:
             if seat.free == True:
-                seat.occupant == name
+                seat.set_occupant(name)
                 break
     
     def left_capacity(self) -> int:
+        """
+        Count how many free seat are available
+        """
+
         free_seats: int = 0
         for seat in self.seats:
             if seat.free == True:
